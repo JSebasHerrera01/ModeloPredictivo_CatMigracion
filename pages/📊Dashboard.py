fa15ad3a59b39d0d3242d04f8d05fcb74c334ccb
Juan Sebastian Herrera <sebastianherrera.2015@gmail.com>
2024-01-25 13:58:48 -0500

    Update 📊Dashboard.py

--- pages/📊Dashboard.py
+++ pages/📊Dashboard.py
@@ -5,12 +5,15 @@ st.markdown("<h1 style='text-align: center;'>Visualización de Flujos Migratorio
 
 # Mostrar el Iframe
 iframe_code = """
-<div style="display: flex; justify-content: center; align-items: center; height: 80vh; width: 60vw; overflow: hidden; margin: auto;">
-    <iframe title="AnalisisExtranjeros_Entradas_24/01" style="width: 100%; height: 100%; margin-left: -50px;" 
+<div style="display: flex; justify-content: center; align-items: center; height: 80vh; width: 60vw; overflow: hidden; margin: auto; background-color: red;">
+    <iframe title="AnalisisExtranjeros_Entradas_24/01" style="width: 100%; height: 100%;" 
     src="https://app.powerbi.com/reportEmbed?reportId=87fc7844-5a79-4ee7-acf7-8ed15a2938b8&autoAuth=true&ctid=bce86336-c485-448d-94ef-1c2df68ef035" 
     frameborder="0" allowFullScreen="true"></iframe>
 </div>
 """
+
+# Mostrar el iframe centrado y ajustado en la aplicación
+st.markdown(iframe_code, unsafe_allow_html=True)
 
 # Mostrar el iframe centrado y ajustado en la aplicación
 st.markdown(iframe_code, unsafe_allow_html=True)

ef36e31fe877a06122bdd3d3b6684bcbe828cb34
Juan Sebastian Herrera <sebastianherrera.2015@gmail.com>
2024-01-25 14:00:28 -0500

    Update 📊Dashboard.py

--- pages/📊Dashboard.py
+++ pages/📊Dashboard.py
@@ -5,10 +5,10 @@ st.markdown("<h1 style='text-align: center;'>Visualización de Flujos Migratorio
 
 # Mostrar el Iframe
 iframe_code = """
-<div style="display: flex; justify-content: center; align-items: center; height: 80vh; width: 60vw; overflow: hidden; margin: auto; background-color: red;">
+<div style="display: flex; justify-content: center; align-items: center; height: 80vh; width: 80vw; overflow: hidden; margin: auto; background-color: lightgray;">
     <iframe title="AnalisisExtranjeros_Entradas_24/01" style="width: 100%; height: 100%;" 
     src="https://app.powerbi.com/reportEmbed?reportId=87fc7844-5a79-4ee7-acf7-8ed15a2938b8&autoAuth=true&ctid=bce86336-c485-448d-94ef-1c2df68ef035" 
-    frameborder="0" allowFullScreen="true"></iframe>
+    frameborder="0"></iframe>
 </div>
 """
 

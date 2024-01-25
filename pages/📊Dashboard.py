9ab58cb0c589ea295259bf2e873744165644e5f1
Juan Sebastian Herrera <sebastianherrera.2015@gmail.com>
2024-01-25 13:28:46 -0500

    Update 📊Dashboard.py

--- pages/📊Dashboard.py
+++ pages/📊Dashboard.py
@@ -6,7 +6,7 @@ st.title("Visualización de Flujos Migratorios en Colombia (Entradas al pais)")
 # Mostrar el Iframe
 iframe_code = """
 <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-    <iframe title="AnalisisExtranjeros_Entradas_24/01" width="1400" height="600.25" 
+    <iframe title="AnalisisExtranjeros_Entradas_24/01" width="100%" height="100%" 
     src="https://app.powerbi.com/reportEmbed?reportId=87fc7844-5a79-4ee7-acf7-8ed15a2938b8&autoAuth=true&ctid=bce86336-c485-448d-94ef-1c2df68ef035" 
     frameborder="0" allowFullScreen="true"></iframe>
 </div>

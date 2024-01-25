143345ae5d233fd64735a0f9e3d0105aa9001106
Juan Sebastian Herrera <sebastianherrera.2015@gmail.com>
2024-01-25 13:50:16 -0500

    Update 📊Dashboard.py

--- pages/📊Dashboard.py
+++ pages/📊Dashboard.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 # Código Streamlit
-
+st.title("Visualización de Flujos Migratorios en Colombia (Entradas al pais)")
 st.markdown("<h1 style='text-align: center;'>Visualización de Flujos Migratorios en Colombia (Entradas al pais)</h1>", unsafe_allow_html=True)
 
 # Mostrar el Iframe
 iframe_code = """
-<div style="display: flex; justify-content: center; align-items: center; height: 80vh; width: 80vw; overflow: hidden; margin: 0;">
+<div style="display: flex; justify-content: center; align-items: center; height: 80vh; width: 80vw; overflow: hidden; margin: auto;">
     <iframe title="AnalisisExtranjeros_Entradas_24/01" style="width: 100%; height: 100%; margin: 0;" 
     src="https://app.powerbi.com/reportEmbed?reportId=87fc7844-5a79-4ee7-acf7-8ed15a2938b8&autoAuth=true&ctid=bce86336-c485-448d-94ef-1c2df68ef035" 
     frameborder="0" allowFullScreen="true"></iframe>
